e3b245abc39f6ee81f7b72733c77412a34335873
구정은 <jemilykoo@gmail.com>
2024-11-26 21:40:27 +0900

    finish

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // const { VITE_NAVER_API_URL } = import.meta.env
@@ -28,7 +27,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), vueJsx(), vueDevTools(), nightwatchPlugin()],
+  plugins: [vue(), vueJsx(), nightwatchPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
